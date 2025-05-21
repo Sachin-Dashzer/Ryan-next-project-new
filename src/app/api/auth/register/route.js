c3b08d3e39a019329909285587035f46687dfd10
sachin <sachin8287037611@gmail.com>
2025-05-21 18:12:05 +0530

    login api implemented

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,14 +1,16 @@
 
+
 import { NextResponse } from "next/server";
 import { asyncHandler } from "@/lib/asyncHandler";
-import { loginUser } from "@/controllers/authController";
+import { registerUser } from "@/controllers/authController";
+
+
 
 export const POST = asyncHandler(async(req) =>{
 
 
     const body = await req.json();
-    const data = await loginUser(body);
+    const data = await registerUser(body);
 
-    return NextResponse.json( data , {status : 201});
-
+    return NextResponse.json(data , {status : 201});
 })
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,5 @@
-import {User} from "@/models/users.js";
+import { User } from "@/models/users.js";
 import bcrypt from "bcryptjs";
-
 
 export const registerUser = async ({ name, email, password }) => {
   if (!name || !email || !password) {
@@ -13,21 +12,13 @@ export const registerUser = async ({ name, email, password }) => {
     throw new Error("Email already exists");
   }
 
-
   const newPassword = await bcrypt.hash(password, 10);
-
-
 
   const user = await User.create({
     name,
     email,
-    password : newPassword,
+    password: newPassword,
   });
-
-
-
-
-
 
   return {
     message: "user added succesfully",
@@ -37,4 +28,32 @@ export const registerUser = async ({ name, email, password }) => {
       email: user.email,
     },
   };
+};
+
+export const loginUser = async ({ email, password }) => {
+  if (!email || !password) {
+    throw new Error("Please fill all the fields");
+  }
+
+  const user = await User.findOne({email});
+
+  if(!user){
+    throw new Error("User not found");
+  }
+
+  const checkUser = await bcrypt.compare(password , user.password);
+
+  if(!checkUser){
+    throw new Error("Invalid Credentials")
+  }
+
+  return {
+    message : "user registered successfully",
+    User : {
+      email : email
+    }
+  }
+
+
+
 };
