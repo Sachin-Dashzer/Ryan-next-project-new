5392dd1fa0ee0399035546a7bf9e06aad28b077a
sachin <sachin8287037611@gmail.com>
2025-08-11 06:41:36 +0530

    testing

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -25,7 +25,6 @@ export default function Home() {
         </div>
       </div>
 
-      <h1 className="text-8xl">Every thing is fantastic</h1>
 
       <HairTransplantPage />
       <WhyChooseRyanClinic />
