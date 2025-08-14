82e0c7fecbaf54f211c8cf4cf9ff79070b162238
sachin <sachin8287037611@gmail.com>
2025-08-14 10:49:14 +0530

    update website banner

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -7,15 +7,15 @@ import OurResults from "./home/ourResults";
 import TurkeySpecialists from "./home/turkeySpecialists"
 import OurBranches from "./home/ourBranches";
 import Testimonials from "./home/testimonial";
-
+import Banner from "../../../public/uploads/banner.jpg"
 
 export default function Home() {
   return (
     <>
       <div className="homeBanner w-full ">
-        <div className="bannerImage w-full h-full ">
+        <div className="bannerImage w-full h-160 ">
           <Image
-            src="https://res.cloudinary.com/dha2ecdnn/image/upload/v1742209534/banner_krdbmt.jpg"
+            src={Banner}
             alt="Ryan Clinic"
             width={100}
             height={100}
